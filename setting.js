38dceb0d7d8f0b56be317b76eee3142d0bc81fb1
FARUQOFC <113996402+Faruq51@users.noreply.github.com>
2022-10-07 22:09:43 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -10,17 +10,17 @@ global.Api = {
 global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
 
 global.smm = {
-  nomor : '6283102650464',
-  atasnama : 'Ahmad Nur Fadhil'
+  nomor : '6285807264974',
+  atasnama : 'Fewler'
 }
 
 // Other
-global.owner = ['6283102650464',"6282135724604"]
-global.premium = ['6283102650464']
+global.owner = ['6285807264974',"6283150915460"]
+global.premium = ['6285807264974']
 global.sponsor = 'no sponsors in:('
 global.packname = 'Crizzy-Bot'
-global.footer = '© 2022 Fadhil Graphy'
-global.author = 'Owner : Fadhil Graphy\nIg1 : @fdhlgrphy\nIg2 : @nr.fdhl__'
+global.footer = '© 2022 By Whatsapp.com'
+global.author = 'Owner : Memek'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.act = {
 		wait: 'please wait a moment, processing data...',
@@ -37,7 +37,7 @@ global.act = {
 }
 
 global.thumb = fs.readFileSync('./lib/crizzy.jpg')
-global.crizzy = { url: "https://g.top4top.io/p_2443v88g61.jpg"}
+global.crizzy = { url: "https://telegra.ph/file/73596f3f8a69213c58c36.jpg"}
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
